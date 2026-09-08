c2f86849f072e2bafad0b88e129b93cdb628b53c
douxxtech <douxx@douxx.tech>
2026-09-08 15:14:53 +0200

    updated custom commands to be in the v2 format

--- handlers/wtt-stop.cmd
+++ handlers/wtt-stop.cmd
@@ -1,7 +1,12 @@
 #!/*/wtt-stop
-# wtt-stop
-#   Stops the running WS -> TCP bridge
-#   Examples:
-#     wtt-stop
+#> syntax ""
+#> short_help "Stops the WebSocket-to-TCP bridge"
+#? Stops the running WS -> TCP bridge
+#?
+#? Example:
+#?   - wtt-stop
+#?
+#? Repository: https://github.com/douxxtech/bw_wtt
+
 
 < /opt/BotWave/scripts/wtt/stop.sh
--- handlers/wtt.cmd
+++ handlers/wtt.cmd
@@ -1,9 +1,17 @@
 #!/*/wtt
-# wtt [TCP PORT]
-#   Starts a bridge between the remote command port and a TCP socket
-#   Default TCP port: 9940
-#   Examples:
-#     wtt
-#     wtt 9950
+#> syntax "[WTT_PORT]"
+#> short_help "Starts a WebSocket-to-TCP bridge"
+#? Starts a bridge between the remote command port and a TCP socket on [WTT_PORT]
+#? Default TCP port: 9940
+#?
+#? Examples:
+#?   - wtt
+#?   - wtt 9950
+#?
+#? Environment variables:
+#?   - REMOTE_CMD_PORT (): The remote cmd port to bridge commands to. If unset, the application won't start
+#?   - WTT_PORT (9940): The port to listen for TCP connections to
+#?
+#? Repository: https://github.com/douxxtech/bw_wtt
 
 < /opt/BotWave/scripts/wtt/start.sh {BW_ARGV1}
