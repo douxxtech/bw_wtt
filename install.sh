86cbb698a04cc3d456217675f444527badd98ce2
douxxtech <douxx@douxx.tech>
2026-09-08 18:17:25 +0200

    install.sh also asks if we want to auto-start the remote cli

--- install.sh
+++ install.sh
@@ -15,11 +15,22 @@ chmod +x "$BW_INSTALL/scripts/wtt/"*.sh
 
 cp handlers/* "$BW_INSTALL/handlers/"
 
-read -p "Do you want to auto-start WTT? [y/N]: " autostart
+read -p "Do you want to auto-start WTT? [Y/n]: " autostart
 
-if [[ "${autostart,,}" == "y" ]]; then
+if [[ "${autostart,,}" != "n" ]]; then
     echo "wtt" > "$BW_INSTALL/handlers/l_onready_wtt_start.shdl"
     echo "wtt" > "$BW_INSTALL/handlers/s_onready_wtt_start.shdl"
+fi
+
+read -p "Do you want to auto-start the remote cmd on port 9939? [Y/n]" autoremote
+
+if [[ "${autoremote,,}" != "n" ]]; then
+    cat >> "$BW_INSTALL/.env" << EOF
+
+# bw_wtt settings start
+REMOTE_CMD_PORT=immutable(9939)
+# bw_wtt settings end
+EOF
 fi
 
 echo "Installed."
